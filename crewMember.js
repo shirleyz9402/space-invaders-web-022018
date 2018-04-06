b7122216acbfa508cd278457e94b0cd0e3b0cc02
shirleyz9402 <shirleyz9402@yahoo.com>
2018-04-06 09:47:38 -0400

    Done.

--- crewMember.js
+++ crewMember.js
@@ -1,1 +1,30 @@
-class CrewMember {}
+class CrewMember {
+  constructor(position, currentShip = 'Looking for a Rig'){
+    this.position = position
+    this.currentShip = currentShip
+  }
+  engageWarpDrive(){
+    if (this.currentShip != 'Looking for a Rig' && this.position === 'Pilot') {
+      return 'engaged'
+    }
+    else {
+      return 'had no effect'
+    }
+  }
+  setsInvisibility(){
+    if (this.currentShip != 'Looking for a Rig' && this.position === 'Defender') {
+      return this.currentShip.cloaked = true
+    }
+    else {
+      return 'had no effect'
+    }
+  }
+  chargePhasers(){
+    if (this.currentShip != 'Looking for a Rig' && this.position === 'Gunner') {
+      return this.currentShip.phasersCharged = true
+    }
+    else {
+      return 'had no effect'
+    }
+  }
+}
--- spaceship.js
+++ spaceship.js
@@ -1,1 +1,22 @@
-class Spaceship {}
+class Spaceship {
+  constructor(name, crew, phasers, shields){
+    this.name = name
+    this.phasers = phasers
+    this.crew = crew
+    this.shields = shields
+    this.cloaked = false
+    this.warpDrive = 'disengaged'
+    this.phasersCharge = 'uncharged'
+    if (crew.length === 0){
+      this.docked = true
+    }
+    else {
+      this.docked = false
+    }
+    this.assignShip()
+  }
+  assignShip(){
+    this.crew.forEach(member => (member.currentShip = this))
+  }
+
+}
